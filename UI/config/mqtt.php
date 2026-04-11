0888558d082cd79fbe897db62ef76f3990a71aa5
Krystian Ziaja <krystianziaja02@gmail.com>
2026-04-11 12:16:09 +0200

    MQTT configs

--- UI/config/mqtt.php
+++ UI/config/mqtt.php
@@ -1,17 +1,18 @@
 <?php
-
+// config/mqtt.php
 return [
     'default' => [
-        'host'      => env('MQTT_HOST'),
-        'port'      => env('MQTT_PORT'),
-        'client_id' => env('MQTT_CLIENT_ID'),
+        'host' => env('MQTT_HOST'),
+        'port' => env('MQTT_PORT'),
+        'protocol_version' => \PhpMqtt\Client\MqttClient::MQTT_3_1_1, // <-- KLUCZOWA ZMIANA
         'connection_settings' => [
             'tls' => [
-                'ca_file'   => env('MQTT_CA_PATH'),
-                'cert_file' => env('MQTT_CERT_PATH'),
-                'private_key_file' => env('MQTT_KEY_PATH'),
+                'ca_file' => env('MQTT_CA_FILE'),
+                'cert_file' => env('MQTT_CERT_FILE'),
+                'private_key_file' => env('MQTT_KEY_FILE'),
                 'verify_peer' => true,
             ],
+            'connect_timeout' => 50, // Skróć timeout, żeby szybciej widzieć błędy
         ],
     ],
 ];
